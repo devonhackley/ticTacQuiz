ef24bd1948cd6eafe89cda663c83390d1420b790
Devon Hackley <devonhackley@gmail.com>
2019-04-15 15:02:40 -0700

    more game logic - grid

--- js/game.js
+++ js/game.js
@@ -5,7 +5,7 @@ const Game = function(quiz, playerOne, playerTwo) {
     this.quiz = quiz;
     this.playerOne = playerOne;
     this.playerTwo = playerTwo;
-    this.questionBank = {};
+    this.questionBank = this.createQuestionBank();
     // Setup grid
     this.grid = new Grid();
     this.grid.setUpGrid();
@@ -14,18 +14,30 @@ const Game = function(quiz, playerOne, playerTwo) {
 
 Game.prototype.createQuestionBank = function(){
     if(localStorage['quiz']) {
-        this.questionBank = JSON.parse(localStorage['quiz']);
+        return JSON.parse(localStorage['quiz']);
     } else {
         console.log('No local storage');
+        return;
     }
 };
 
 Game.prototype.playGame = function(){
     // figure out whose turn it is(active player)
+    let activePlayer = '';
+    if(this.isPlayerOneTurn) {
+        activePlayer = this.playerOne;
+    } else {
+        activePlayer = this.playerTwo;
+    }
+
     // populate grid and populate grid cell with questions  : call from grid.js
+    this.grid.populateGrid(this.questionBank);
     // Get user cell selection
-    // Show question in cell: question.js
+    var userCell = this.grid.getCellSelection();
+    // Show question in cell: grid.js
+    this.grid.showQuestion(userCell);
     // handle user selection from question (check to see right or wrong)
+
     // check win conditions
     // update grid
     // switch turns
@@ -35,4 +47,7 @@ Game.prototype.switchTurns = function(){
     this.isPlayerOneTurn = !this.isPlayerOneTurn;
 };
 
+Game.prototype.handleUserSelection = function(){
+
+}
 

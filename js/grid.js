c1c8cc56833683b33eeda42418e398db5a6f9ec8
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-04-16 07:58:02 -0700

    add cell constructor

--- js/grid.js
+++ js/grid.js
@@ -1,11 +1,25 @@
 'use strict';
+const Cell = function(mcQuestions, tfQuestion) {
+    this.mcQuestions = [];
+    this.tieBreakerQuestion = tfQuestion;
+    this.winner;
+    this.winnerIcon;
+};
 
 const Grid = function(){
     console.log('grid');
+    this.cells = [];
 };
 
 Grid.prototype.setUpGrid = function(){
-    console.log('grid is setup');
+  //first Grid Instance
+  //get Grid questions
+  if(!this.cells.length) {
+    console.log('I am empty');
+  } else {
+    console.log('I am not empty');
+    this.populateGrid();
+  }
 };
 
 Grid.prototype.populateGrid = function(bank){
@@ -28,3 +42,12 @@ Grid.prototype.checkWinConditions = function(){
 Grid.prototype.updateGrid = function(){
     console.log('grid has been updated');
 };
+
+Grid.prototype.randomizeArray = function(arr) {
+    //hkl
+};
+
+
+var grid = new Grid();
+grid.setUpGrid();
+

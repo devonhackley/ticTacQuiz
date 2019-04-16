146027b8e86bd7ab9a24072e83bff3aded8ad563
Peter Tynan <pettynan@amazon.com>
2019-04-16 12:05:33 -0700

    finished questionType switch functionality

--- js/create-quiz.js
+++ js/create-quiz.js
@@ -9,40 +9,35 @@ function handleNewQuestionSubmit(event) {
     var quizName = event.target.quizName.value;
     var questionType = event.target.questionType.value;
     var questionQuestion = event.target.questionQuestion.value;
-    var questionResponses = [event.target.answer1.value, event.target.answer2.value, event.target.answer3.value, event.target.answer4.value];
+    if (questionType === 'TrueFalse') {
+        var questionResponses = ['true', 'false'];
+    } else {
+        questionResponses = [event.target.answer1.value, event.target.answer2.value, event.target.answer3.value, event.target.answer4.value];
+        event.target.answer1.value = null;
+        event.target.answer2.value = null;
+        event.target.answer3.value = null;
+        event.target.answer4.value = null;
+    }
     var correctAnswer = event.target.correctAnswer.value;
 
-
-    event.target.quizname.value = null;
+    event.target.quizName.value = null;
     event.target.questionType.value = null;
     event.target.questionQuestion.value = null;
-    event.target.answer1.value = null;
-    event.target.answer2.value = null;
-    event.target.answer3.value = null;
-    event.target.answer4.value = null;
+
+
     event.target.correctAnswer.value = null;
-
-
-
 
     // eslint-disable-next-line no-undef
     new Question(quizName, questionType, questionQuestion, questionResponses, correctAnswer);
 
 }
 
-
-
-
-
 function questionTypeHandler(event) {
-
-
-    // if (event.target.name === questionType) {
-
     responseSection.innerHTML = '';
 
     var questionType = event.target.value;
-
+    var inputEl;
+    var labelEl;
 
     if (questionType === 'TrueFalse') {
         var radioEl = document.createElement('input');
@@ -50,6 +45,7 @@ function questionTypeHandler(event) {
         radioEl.setAttribute('name', 'correctAnswer');
         radioEl.setAttribute('type', 'radio');
         radioEl.setAttribute('value', '0');
+        radioEl.setAttribute('checked', '');
         responseSection.append('True ', radioEl, document.createElement('br'));
         radioEl = document.createElement('input');
         radioEl.setAttribute('name', 'correctAnswer');
@@ -57,28 +53,27 @@ function questionTypeHandler(event) {
         radioEl.setAttribute('value', '1');
         responseSection.append('False', radioEl);
 
-        
-
     } else if (questionType === 'MultipleChoice'){
-
-        var inputEl = document.createElement('input');
-        radioEl = document.createElement('input');
-        var labelEl = document.createElement('label');
-        labelEl.setAttribute('for', 'answer1');
-        labelEl.textContent = 'Answer 1:';
-        responseSection.append(labelEl);
-        inputEl.setAttribute('name', 'answer1');
-        inputEl.setAttribute('type', 'text');
-        inputEl.setAttribute('placeholder', 'Type response here...');
-        responseSection.append(inputEl);
-        radioEl.setAttribute('name', 'correctAnswer');
-        radioEl.setAttribute('type', 'radio');
-        radioEl.setAttribute('value', '0');
-        
+        for (var i = 0 ; i < 4 ; i++) {
+            inputEl = document.createElement('input');
+            radioEl = document.createElement('input');
+            labelEl = document.createElement('label');
+            labelEl.setAttribute('for', `answer${i + 1}`);
+            labelEl.textContent = `Answer ${i + 1}:`;
+            responseSection.append(labelEl);
+            inputEl.setAttribute('name', `answer${i + 1}`);
+            inputEl.setAttribute('type', 'text');
+            inputEl.setAttribute('placeholder', 'Type response here... ');
+            responseSection.append(inputEl);
+            radioEl.setAttribute('name', 'correctAnswer');
+            radioEl.setAttribute('type', 'radio');
+            radioEl.setAttribute('value', i);
+            if (i === 0) {
+                radioEl.setAttribute('checked', '');
+            }
+            responseSection.append(radioEl, document.createElement('br'));
+        }
     }
-
-    // }
-
 }
 
 

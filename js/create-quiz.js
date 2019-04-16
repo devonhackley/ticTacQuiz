e4ec4f43b0d2bc5e2f885b45332b528523d37632
Peter Tynan <pettynan@amazon.com>
2019-04-16 11:28:47 -0700

    functioning true/false question type

--- js/create-quiz.js
+++ js/create-quiz.js
@@ -1,4 +1,6 @@
 var newQuestionForm = document.getElementById('newQuestionForm');
+var responseSection = document.getElementById('responseSection');
+var questionTypeSection = document.getElementById('questionTypeSection');
 
 // Event handler for the submission of a new question.
 
@@ -23,9 +25,69 @@ function handleNewQuestionSubmit(event) {
 
 
 
+    // eslint-disable-next-line no-undef
     new Question(quizName, questionType, questionQuestion, questionResponses, correctAnswer);
 
 }
 
 
+
+
+
+function questionTypeHandler(event) {
+
+
+    // if (event.target.name === questionType) {
+
+    responseSection.innerHTML = '';
+
+    var questionType = event.target.value;
+
+
+    if (questionType === 'TrueFalse') {
+        var radioEl = document.createElement('input');
+
+        radioEl.setAttribute('name', 'correctAnswer');
+        radioEl.setAttribute('type', 'radio');
+        radioEl.setAttribute('value', '0');
+        responseSection.append('True ', radioEl, document.createElement('br'));
+        radioEl = document.createElement('input');
+        radioEl.setAttribute('name', 'correctAnswer');
+        radioEl.setAttribute('type', 'radio');
+        radioEl.setAttribute('value', '1');
+        responseSection.append('False', radioEl);
+
+        
+
+    } else if (questionType === 'MultipleChoice'){
+
+        var inputEl = document.createElement('input');
+        radioEl = document.createElement('input');
+        var labelEl = document.createElement('label');
+        labelEl.setAttribute('for', 'answer1');
+        labelEl.textContent = 'Answer 1:';
+        responseSection.append(labelEl);
+        inputEl.setAttribute('name', 'answer1');
+        inputEl.setAttribute('type', 'text');
+        inputEl.setAttribute('placeholder', 'Type response here...');
+        responseSection.append(inputEl);
+        radioEl.setAttribute('name', 'correctAnswer');
+        radioEl.setAttribute('type', 'radio');
+        radioEl.setAttribute('value', '0');
+        
+    }
+
+    // }
+
+}
+
+
+
+
+
+
+
+
 newQuestionForm.addEventListener('submit', handleNewQuestionSubmit);
+questionTypeSection.addEventListener('input', questionTypeHandler);
+

c8a2140ecf738b2fa4f0a1777518a542faaa8380
Devon Hackley <devonhackley@gmail.com>
2019-04-15 17:19:24 -0700

    working thru game start logic

--- js/gamestart.js
+++ js/gamestart.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const gameSelect = document.getElementById('questionsSelection');
+const gameStartForm = document.getElementById('gameStartForm');
+function populateQuizzes(){
+
+}
+
+function handleGameStartForm(event){
+    event.preventDefault();
+
+    // create new game from inputs
+    const newGame = new Game();
+
+    //reset form
+    gameStartForm.reset();
+}

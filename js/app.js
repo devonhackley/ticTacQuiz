af1e275e1e8763fe2a07e2a1d13edb4521858abc
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-04-17 17:40:19 -0700

    resolve button selctor bug

--- js/app.js
+++ js/app.js
@@ -264,9 +264,6 @@ var showQuestionForm = function(question) {
     elSubmit.setAttribute('id', 'user-response-button');
     elSubmit.textContent = 'Submit Answer';
     questionShowForm.appendChild(elSubmit);
-    userResponseButton = document.getElementById('user-response-button');
-    console.log(userResponseButton);
-
     console.log(question.correctAnswer);
 
 };
